1807478884708450c653397ce0123db38fe51353
krzysiek <krzysztof.muhleisen@gmail.com>
2022-03-16 20:49:47 +0100

    IDK

--- tests/opt_transform.test.cpp
+++ tests/opt_transform.test.cpp
@@ -1,4 +1,5 @@
 #include <catch.hpp>
+#include <cpp_pipelines/functions.hpp>
 #include <cpp_pipelines/opt.hpp>
 
 using namespace cpp_pipelines;
@@ -14,7 +15,9 @@ struct God
 
 TEST_CASE("opt::transform", "[opt]")
 {
-    REQUIRE((std::optional{ "Poseidon"s } >>= opt::transform(&std::string::size)) == std::optional{ 8 });
-    auto god = God{ "Hades" };
-    REQUIRE((std::addressof(god >>= opt::lift >>= opt::transform(&God::name) >>= opt::value)) == std::addressof(god.name));
+    REQUIRE((opt::lift("Poseidon"s) >>= opt::transform(&std::string::size)) == opt::lift(8));
+    REQUIRE((std::optional<std::string>{} >>= opt::transform(&std::string::size)) == std::nullopt);
+    const auto god = God{ "Hades" };
+    REQUIRE((std::addressof(opt::lift(god) >>= opt::transform(identity) >>= opt::value)) == std::addressof(god));
+    REQUIRE((std::addressof(opt::lift(god) >>= opt::transform(&God::name) >>= opt::value)) == std::addressof(god.name));
 }
