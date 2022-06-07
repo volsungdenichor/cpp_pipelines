299e446972cb026213a9f625d745c2c8842be84b
krzysiek <krzysztof.muhleisen@gmail.com>
2022-06-07 20:41:12 +0200

    scope functions

---
+++ include/cpp_pipelines/scope_functions.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <cpp_pipelines/pipeline.hpp>
+
+namespace cpp_pipelines
+{
+namespace detail
+{
+struct inspect_fn
+{
+    template <class Func>
+    struct impl
+    {
+        Func func;
+
+        template <class T>
+        constexpr decltype(auto) operator()(T&& item) const
+        {
+            invoke(func, item);
+            return to_return_type(std::forward<T>(item));
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func func) const
+    {
+        return make_pipeline(impl<Func>{ std::move(func) });
+    }
+};
+
+struct apply_fn
+{
+    template <class Func>
+    struct impl
+    {
+        Func func;
+
+        template <class T>
+        constexpr T& operator()(T& item) const
+        {
+            invoke(func, item);
+            return item;
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func func) const
+    {
+        return make_pipeline(impl<Func>{ std::move(func) });
+    }
+};
+
+struct with_fn
+{
+    template <class Func>
+    struct impl
+    {
+        Func func;
+
+        template <class T>
+        constexpr T operator()(T item) const
+        {
+            invoke(func, item);
+            return item;
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func func) const
+    {
+        return make_pipeline(impl<Func>{ std::move(func) });
+    }
+};
+
+}  // namespace detail
+
+static constexpr inline auto inspect = detail::inspect_fn{};
+static constexpr inline auto tap = inspect;
+
+static constexpr inline auto apply = detail::apply_fn{};
+static constexpr inline auto with = detail::with_fn{};
+
+}  // namespace cpp_pipelines
