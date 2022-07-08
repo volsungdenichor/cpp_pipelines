a547bb50b8c035d23ee133dc8c6cc349add5f6f2
krzysiek <krzysztof.muhleisen@gmail.com>
2022-07-08 21:40:53 +0200

    iterable refactored

--- include/cpp_pipelines/iterable.hpp
+++ include/cpp_pipelines/iterable.hpp
@@ -5,48 +5,35 @@
 
 namespace cpp_pipelines
 {
+namespace detail
+{
+template <class T>
+struct i_iterator
+{
+    virtual ~i_iterator() = default;
+    virtual T deref() const = 0;
+    virtual void inc() = 0;
+    virtual bool is_equal(const i_iterator& other) const = 0;
+    virtual std::unique_ptr<i_iterator> clone() const = 0;
+};
+
+template <class T>
+struct i_range
+{
+    virtual ~i_range() = default;
+    virtual std::unique_ptr<i_iterator<T>> begin() const = 0;
+    virtual std::unique_ptr<i_iterator<T>> end() const = 0;
+};
+
 template <class T>
 struct iterable_base
 {
     template <class Range>
-    constexpr iterable_base(Range&& range)
-    {
-        auto r = all(std::forward<Range>(range));
-        impl = std::make_unique<range_wrapper<decltype(r)>>(std::move(r));
-    }
-
-    constexpr auto begin() const
-    {
-        return iterator_interface{ iter{ impl->begin() } };
-    }
-
-    constexpr auto end() const
-    {
-        return iterator_interface{ iter{ impl->end() } };
-    }
-
-    struct i_iterator
-    {
-        virtual ~i_iterator() = default;
-        virtual T deref() const = 0;
-        virtual void inc() = 0;
-        virtual bool is_equal(const i_iterator& other) const = 0;
-        virtual std::unique_ptr<i_iterator> clone() const = 0;
-    };
-
-    struct i_range
-    {
-        virtual ~i_range() = default;
-        virtual std::unique_ptr<i_iterator> begin() const = 0;
-        virtual std::unique_ptr<i_iterator> end() const = 0;
-    };
-
-    template <class Range>
-    struct range_wrapper : public i_range
+    struct range_wrapper : public i_range<T>
     {
         Range range;
 
-        struct iterator : public i_iterator
+        struct iterator : public i_iterator<T>
         {
             using inner_iter = iterator_t<Range>;
             inner_iter it;
@@ -66,12 +53,12 @@ struct iterable_base
                 ++it;
             }
 
-            bool is_equal(const i_iterator& other) const override
+            bool is_equal(const i_iterator<T>& other) const override
             {
                 return it == static_cast<const iterator&>(other).it;
             }
 
-            std::unique_ptr<i_iterator> clone() const override
+            std::unique_ptr<i_iterator<T>> clone() const override
             {
                 return std::make_unique<iterator>(it);
             }
@@ -82,12 +69,12 @@ struct iterable_base
         {
         }
 
-        std::unique_ptr<i_iterator> begin() const override
+        std::unique_ptr<i_iterator<T>> begin() const override
         {
             return std::make_unique<iterator>(std::begin(range));
         }
 
-        std::unique_ptr<i_iterator> end() const override
+        std::unique_ptr<i_iterator<T>> end() const override
         {
             return std::make_unique<iterator>(std::end(range));
         }
@@ -95,14 +82,14 @@ struct iterable_base
 
     struct iter
     {
-        std::unique_ptr<i_iterator> it;
+        std::unique_ptr<i_iterator<T>> it;
 
         iter()
             : it{}
         {
         }
 
-        iter(std::unique_ptr<i_iterator> it)
+        iter(std::unique_ptr<i_iterator<T>> it)
             : it{ std::move(it) }
         {
         }
@@ -133,13 +120,32 @@ struct iterable_base
         }
     };
 
-    std::unique_ptr<i_range> impl;
+    template <class Range>
+    constexpr iterable_base(Range&& range)
+    {
+        auto r = all(std::forward<Range>(range));
+        impl = std::make_unique<range_wrapper<decltype(r)>>(std::move(r));
+    }
+
+    constexpr auto begin() const
+    {
+        return iterator_interface{ iter{ impl->begin() } };
+    }
+
+    constexpr auto end() const
+    {
+        return iterator_interface{ iter{ impl->end() } };
+    }
+
+    std::unique_ptr<i_range<T>> impl;
 };
 
+}  // namespace detail
+
 template <class T>
-struct iterable : public view_interface<iterable_base<T>>
+struct iterable : public view_interface<detail::iterable_base<T>>
 {
-    using base_type = view_interface<iterable_base<T>>;
+    using base_type = view_interface<detail::iterable_base<T>>;
 
     template <class Range>
     constexpr iterable(Range&& range)
