135b34f8566ef8963af3da43a26408215c6ac308
krzysiek <krzysztof.muhleisen@gmail.com>
2022-07-13 20:11:26 +0200

    tpl namespace

---
+++ include/cpp_pipelines/tpl.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <cpp_pipelines/pipeline.hpp>
+
+namespace cpp_pipelines::tpl
+{
+namespace detail
+{
+struct transform_fn
+{
+    template <class Func>
+    struct impl
+    {
+        Func func;
+
+        template <class... Args>
+        constexpr auto operator()(const std::tuple<Args...>& tuple) const
+        {
+            return call(tuple, std::index_sequence_for<Args...>{});
+        }
+
+        template <class K, class V>
+        constexpr auto operator()(const std::pair<K, V>& pair) const
+        {
+            return std::pair{
+                invoke(func, std::get<0>(pair)),
+                invoke(func, std::get<1>(pair))
+            };
+        }
+
+        template <class Tuple, std::size_t... I>
+        constexpr auto call(const Tuple& tuple, std::index_sequence<I...>) const
+        {
+            return std::tuple{ invoke(func, std::get<I>(tuple))... };
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func func) const
+    {
+        return make_pipeline(impl<Func>{ std::move(func) });
+    }
+};
+
+struct for_each_fn
+{
+    template <class Func>
+    struct impl
+    {
+        Func func;
+
+        template <class... Args>
+        constexpr void operator()(const std::tuple<Args...>& tuple) const
+        {
+            return call(tuple, std::index_sequence_for<Args...>{});
+        }
+
+        template <class K, class V>
+        constexpr void operator()(const std::pair<K, V>& pair) const
+        {
+            return call(pair, std::index_sequence_for<K, V>{});
+        }
+
+        template <class Tuple, std::size_t... I>
+        constexpr void call(const Tuple& tuple, std::index_sequence<I...>) const
+        {
+            (invoke(func, std::get<I>(tuple)), ...);
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func func) const
+    {
+        return make_pipeline(impl<Func>{ std::move(func) });
+    }
+};
+}  // namespace detail
+
+static constexpr inline auto transform = detail::transform_fn{};
+static constexpr inline auto for_each = detail::for_each_fn{};
+}  // namespace cpp_pipelines::tpl
