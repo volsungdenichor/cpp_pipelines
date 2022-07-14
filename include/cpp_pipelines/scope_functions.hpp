1a6cb36e8f759a06622eb09261a3f61acceefb4f
krzysiek <krzysztof.muhleisen@gmail.com>
2022-07-14 17:11:37 +0200

    do_all

--- include/cpp_pipelines/scope_functions.hpp
+++ include/cpp_pipelines/scope_functions.hpp
@@ -72,6 +72,33 @@ struct with_fn
     }
 };
 
+struct do_all_fn
+{
+    template <class... Funcs>
+    struct impl
+    {
+        std::tuple<Funcs...> funcs;
+
+        template <class... Args>
+        constexpr void operator()(Args&&... args) const
+        {
+            call(std::index_sequence_for<Funcs...>{}, std::forward<Args>(args)...);
+        }
+
+        template <class... Args, std::size_t... I>
+        constexpr void call(std::index_sequence<I...>, Args&&... args) const
+        {
+            (invoke(std::get<I>(funcs), std::forward<Args>(args)...), ...);
+        }
+    };
+
+    template <class... Funcs>
+    constexpr auto operator()(Funcs... funcs) const
+    {
+        return make_pipeline(impl<Funcs...>{ std::tuple{ std::move(funcs)... } });
+    }
+};
+
 }  // namespace detail
 
 static constexpr inline auto inspect = detail::inspect_fn{};
@@ -79,6 +106,8 @@ static constexpr inline auto tap = inspect;
 
 static constexpr inline auto apply = detail::apply_fn{};
 static constexpr inline auto with = detail::with_fn{};
+
+static constexpr inline auto do_all = detail::do_all_fn{};
 
 template <class T>
 constexpr T create(const std::function<void(T&)>& func)
