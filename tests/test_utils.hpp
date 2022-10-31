ffce0658ae2f2f34caa634acd02153984f20679d
krzysiek <krzysztof.muhleisen@gmail.com>
2022-10-31 20:58:37 +0100

    UT's for map::

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -2,6 +2,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_templated.hpp>
+#include <cpp_pipelines/output.hpp>
 #include <utility>
 
 namespace std
@@ -10,6 +11,12 @@ template <class... L, class... R>
 bool operator==(const pair<L...>& lhs, const pair<R...>& rhs)
 {
     return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <class... Args>
+ostream& operator<<(ostream& os, const vector<Args...>& item)
+{
+    return os << cpp_pipelines::delimit(item, ", ");
 }
 }  // namespace std
 
@@ -41,3 +48,25 @@ auto EqualsRange(const Range& range) -> EqualsRangeMatcher<Range>
 {
     return EqualsRangeMatcher<Range>{ range };
 }
+
+namespace cpp_pipelines
+{
+template <class Impl>
+std::ostream& operator<<(std::ostream& os, const view_interface<Impl>& item)
+{
+    return os << delimit(item, ", ");
+}
+
+template <class Impl, class R>
+bool operator==(const view_interface<Impl>& lhs, const R& rhs)
+{
+    return std::equal(std::begin(lhs), std::end(lhs), std::begin(rhs), std::end(rhs));
+}
+
+template <class L, class Impl>
+bool operator==(const L& lhs, const view_interface<Impl>& rhs)
+{
+    return rhs == lhs;
+}
+
+}  // namespace cpp_pipelines
